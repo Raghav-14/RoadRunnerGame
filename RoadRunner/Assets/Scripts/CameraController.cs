0f52708f97fbf0f2c9881d57b669d89bb1281cc5
Raghav <14raghav.kulkarni@gmail.com>
2020-09-26 14:56:52 +0530

    RoadRunnerGame
    Commit Contains,
    player movment
    running phase
    environment

---
+++ RoadRunner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform lookAt; //player position 
+    private Vector3 startingOffset; //distance between camera and player
+    private Vector3 moveTransfrom;
+
+    //variables for starting transitions
+    private float transition = 0.0f;
+    private float cameraTransitionDuration = 3.0f;
+    private Vector3 cameraOffset = new Vector3 (0, 5, 5); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startingOffset = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTransfrom = lookAt.position + startingOffset;
+        //for X 
+        moveTransfrom.x = 0; //want camera to keep center always
+        //for y 
+        moveTransfrom.y = Mathf.Clamp(moveTransfrom.y, 3.0f, 5.0f);
+
+        if (transition > 1.0f)
+        {
+            transform.position = moveTransfrom;
+        }
+        else
+        {
+            //Lerp Interpolate between 2 points which are first 2 parameters
+            //starting animation 
+            transform.position = Vector3.Lerp(moveTransfrom + cameraOffset,
+                 moveTransfrom, //moving position
+                transition); //between 0 to 1
+            transition += Time.deltaTime * 1 / cameraTransitionDuration; // increase transition value  
+            transform.LookAt(lookAt.position + Vector3.up); //Looking at player while transitioning
+        }
+
+    }
+}
---
+++ RoadRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController charController;
+    public float charSpeed = 10.0f;
+    private Vector3 moveVector; //char moving variable
+    private float verticleVelocity = 0.0f;
+    private float charGravity = 1.0f;
+    private float sideOffsetToMove = 6.0f; // left and right movement 
+
+    private float cameraTransitionDuration = 3.0f; //starting transition
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time < cameraTransitionDuration)
+        {
+            //While there is starting transition of camera then we dont move player left and right  
+            charController.Move(Vector3.forward * Time.deltaTime * charSpeed);
+            return;
+        }
+        //reset to zero
+        moveVector = Vector3.zero;
+        //for left and right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * sideOffsetToMove;
+       // moveVector.x = Mathf.Clamp(moveVector.x, -3, 3);
+        //for up and down
+        if(charController.isGrounded)
+        {
+            verticleVelocity = -0.5f;
+        }
+        else
+        {
+            verticleVelocity -= charGravity * Time.deltaTime;
+        }
+        moveVector.y = verticleVelocity;
+        //for forward and backward
+        moveVector.z = charSpeed;
+       // moveVector = new Vector3((Mathf.Clamp(moveVector.x,-3, 3)), moveVector.y, moveVector.z);
+        charController.Move(moveVector * Time.deltaTime );
+    }
+}
